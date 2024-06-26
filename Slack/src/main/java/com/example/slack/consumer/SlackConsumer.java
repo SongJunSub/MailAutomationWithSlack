617ec5a0ac4e271d93f68bd595d0ae67d7b306cb
Song Jun-Sub <song911130@gmail.com>
2024-06-26 16:21:12 +0900

    application-secret.yml 설정 변경, Slack 메시지 전송 로직 구현

--- Slack/src/main/java/com/example/slack/consumer/SlackConsumer.java
+++ Slack/src/main/java/com/example/slack/consumer/SlackConsumer.java
@@ -3,8 +3,9 @@ package com.example.slack.consumer;
 import com.example.slack.dto.EmailDTO;
 import com.example.slack.entity.EmailHistory;
 import com.example.slack.repository.EmailHistoryRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.example.slack.service.SlackNotificationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -13,11 +14,16 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class SlackConsumer {
 
-    private final ObjectMapper objectMapper;
     private final EmailHistoryRepository emailHistoryRepository;
+    private final SlackNotificationService slackNotificationService;
+
+    @Value("${slack.channel}")
+    private String slackChannelName;
 
     @KafkaListener(topics = "${spring.kafka.template.default-topic}", groupId = "${spring.kafka.consumer.group-id}")
     public void listener(@Payload EmailDTO emailDTO) {
+        StringBuilder stringBuilder = new StringBuilder();
+
         try {
             EmailHistory emailHistory = new EmailHistory(
                     emailDTO.getSenderName(),
@@ -30,6 +36,13 @@ public class SlackConsumer {
             );
 
             emailHistoryRepository.save(emailHistory);
+
+            stringBuilder.append("[").append(emailDTO.getStatus()).append("] 제목 : ").append(emailDTO.getSubject()).append("\n");
+            stringBuilder.append("발송자명 : ").append(emailDTO.getSenderName()).append("\n");
+            stringBuilder.append("발송자 메일 : ").append(emailDTO.getSenderEmail()).append("\n");
+            stringBuilder.append("첨부파일 유무 : ").append(emailDTO.isHasAttachments());
+
+            slackNotificationService.sendSlackMessage(slackChannelName, stringBuilder.toString());
         }
         catch (Exception e) {
             e.printStackTrace();
