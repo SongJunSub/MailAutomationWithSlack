c2853c61c31d9ea0cbf970ad18b1d203a9377345
Song Jun-Sub <song911130@gmail.com>
2024-06-26 16:01:45 +0900

    SlackNotificationService 로직 작성

--- Slack/src/main/java/com/example/slack/service/SlackNotificationService.java
+++ Slack/src/main/java/com/example/slack/service/SlackNotificationService.java
@@ -1,19 +1,47 @@
 package com.example.slack.service;
 
 import com.example.slack.dto.SlackMessageDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import okhttp3.RequestBody;
+import okhttp3.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
 public class SlackNotificationService {
 
-    public void sendSlackMessage(String channel, String message) {
+    private final ObjectMapper objectMapper;
+    private final OkHttpClient okHttpClient = new OkHttpClient();
+    private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+
+    @Value("${slack.oauth.token}")
+    private String slackToken;
+
+    public void sendSlackMessage(String channel, String message) throws JsonProcessingException {
         String slackURL = "https://slack.com/api/chat.postMessage";
 
         SlackMessageDTO slackMessageDTO = new SlackMessageDTO(channel, message);
+
+        RequestBody requestBody = RequestBody.create(objectMapper.writeValueAsString(slackMessageDTO), JSON);
+
+        Request request = new Request.Builder()
+                .url(slackURL)
+                .header("Authorization", "Bearer " + slackToken)
+                .post(requestBody)
+                .build();
+
+        try (Response response = okHttpClient.newCall(request).execute()){
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response);
+            }
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
